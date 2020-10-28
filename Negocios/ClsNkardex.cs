7a07d58e4ed40ae9bdddce4e0051cafaa3f1ddf7
chano <promo201674@gmail.com>
2020-10-27 22:18:14 -0500

    Borrando código innecesario

--- Negocios/ClsNkardex.cs
+++ Negocios/ClsNkardex.cs
@@ -7,22 +7,6 @@ using Entidad;
 
 namespace Negocios {
     public class ClsNkardex {
-        public DataTable MtdListarTablaKardex() {
-            ClsConexionSQL conn = new ClsConexionSQL();
-            DataTable result = new DataTable();
-            SqlDataAdapter adapter = new SqlDataAdapter();
-            SqlCommand command = new SqlCommand();
-            command.Connection = conn.Conectar();
-            command.CommandType = CommandType.StoredProcedure;
-            command.CommandText = "USP_S_ListarKardex";
-            command.ExecuteNonQuery();
-            adapter.SelectCommand = command;
-            adapter.Fill(result);
-            command.Connection = conn.Desconectar();
-
-            return result;
-        }
-
         ClsDkardex datos = new ClsDkardex();
 
         public ArrayList listarKardex() {
